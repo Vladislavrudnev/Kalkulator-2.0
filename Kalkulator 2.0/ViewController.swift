a36b22823dd04873cc9672958a509685b2ee8399
emoge <vlad.rudnev.1992@mail.ru>
2022-05-02 17:12:43 +0600

    Initial Commit

---
+++ Kalkulator 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kalkulator 2.0
+//
+//  Created by владислав Руднев on 02.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
